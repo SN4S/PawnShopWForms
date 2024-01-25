8426786551443124222c9afdd1ef67c87e90fe81
SN4S <san4es772@gmail.com>
2024-01-25 03:34:24 +0200

    Create filedb manager

---
+++ PawnShopFiles/FilesManager.cs
@@ -0,0 +1,12 @@
+namespace PawnShopFiles
+{
+    public class FilesManager
+    {
+        public string clientsFile = "clients.txt";
+        public string workersFile = "workers.txt";
+        public string File = "";
+        public string historyFile = "history.txt";
+        
+        
+    }
+}
