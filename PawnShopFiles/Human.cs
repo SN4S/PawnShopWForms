956a481b2e4d8ae08601cff23738bd88d5df4a09
SN4S <san4es772@gmail.com>
2024-01-25 03:45:01 +0200

    Create basic human class for others derived classes

---
+++ PawnShopFiles/Human.cs
@@ -0,0 +1,26 @@
+namespace PawnShopFiles
+{
+    public class Human
+    {
+        protected string _fname;
+        protected string _sname;
+
+        public string Fname
+        {
+            get => _fname;
+            set => _fname = value;
+        }
+
+        public string Sname
+        {
+            get => _sname;
+            set => _sname = value;
+        }
+        
+        public Human(string fname, string sname)
+        {
+            this._fname = fname;
+            this._sname = sname;
+        }
+    }
+}
